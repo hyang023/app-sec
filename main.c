a90fed2c4bc2a301df383ffd38b1a6b7c0ce45fb
hyang023 <hwy227@nyu.edu>
2019-10-08 22:30:15 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "dictionary.h"
+
+int main(int argc, char *argv[]){
+
+	return 0;
+}
