65773c9120b8820ad0d92d9ecc7317dcfed047d4
hyang023 <hwy227@nyu.edu>
2019-10-09 00:34:40 -0400

    Update main.c

--- main.c
+++ main.c
@@ -20,6 +20,6 @@ int main(int argc, char *argv[]){
 	free(line);
     	fclose(fp);
 	int num_misspelled = check_words(fp, hashtable, misspelled);
-	printf("%d",num_misspelled);
+	printf("num misspelled is %d",num_misspelled);
 	return 0;
 }
