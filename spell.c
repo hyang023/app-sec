9ea7b45ea55f3ac457424826fe915b9446b5ec08
hyang023 <hwy227@nyu.edu>
2019-10-11 00:06:36 -0400

    Update spell.c

--- spell.c
+++ spell.c
@@ -22,10 +22,12 @@ bool check_word(const char* word, hashmap_t hashtable[]) {
     //Set hashmap_t cursor equal to hashmap[bucket].
     hashmap_t cursor = hashtable[hash_val];
     //While cursor is not NULL:
+    char lower_word2[strlen(lower_word);
+    strcpy(lower_word2, lower_word);
     while (cursor != NULL){
         printf("cursor word is \"%s\"\n",cursor->word);
         //If word equals cursor->word:
-        if (lower_word == cursor->word){
+        if (lower_word2 == cursor->word){
             printf("returning 1\n");
             return 1;
         }
