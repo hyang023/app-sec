d133b56773d51c872a5d2ecda76402b5e75eaf70
hyang023 <hwy227@nyu.edu>
2019-10-10 22:15:18 -0400

    Update main.c

--- main.c
+++ main.c
@@ -10,17 +10,16 @@ int main(int argc, char *argv[]){
 	load_dictionary(argv[2], hashtable);
 	char *misspelled[MAX_MISSPELLED];
 	FILE *fp = fopen(argv[1], "r");
-	char *line = NULL;
+	/*char *line = NULL;
 	size_t len = 0;
     	ssize_t read;
 	while ((read = getline(&line, &len, fp)) != -1) {
         	printf("Retrieved line of length %lu :\n", read);
 		printf("%s", line);
-	}
-	free(line);
-    	fclose(fp);
-	FILE *fp2 = fopen(argv[2], "r");
-	int num_misspelled = check_words(fp2, hashtable, misspelled);
+	}*/
+	//free(line);
+    	//fclose(fp);
+	int num_misspelled = check_words(fp, hashtable, misspelled);
 	printf("num misspelled is %d\n",num_misspelled);
 	return 0;
 }
