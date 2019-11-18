39260282634327bc47e06b1eada12716ebaae4f0
hyang023 <hwy227@nyu.edu>
2019-11-17 19:14:58 -0500

    Update spell.c

--- spell.c
+++ spell.c
@@ -26,11 +26,11 @@ char* remove_punctuation(char* word) {
 //function check_word(string word, hashmap hashtable[])
 bool check_word(const char* word, hashmap_t hashtable[]) {
     //check if string is number
-    int isnum = 1;
-    for (int i=0; i<strlen(word); i++){
-      if (!(isdigit(word[i]))) isnum=0;
-    }
-    if (isnum ==1) return 1;
+    //int isnum = 1;
+    //for (int i=0; i<strlen(word); i++){
+    //  if (!(isdigit(word[i]))) isnum=0;
+    //}
+    //if (isnum ==1) return 1;
     //string lower_word = to_lower(word)
     int a = strlen(word);
     char* lower_word = malloc(a+1);
