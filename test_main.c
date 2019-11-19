14fc00cf7ae66128c56f8a9adf917d349680ae54
hyang023 <hwy227@nyu.edu>
2019-11-18 21:34:09 -0500

    Update test_main.c

--- test_main.c
+++ test_main.c
@@ -45,7 +45,6 @@ START_TEST(test_check_word_normal)
     const char* number_word = "12345";
     ck_assert(check_word(correct_word, hashtable));
     ck_assert(!check_word(punctuation_word_2, hashtable));
-    // Test here: What if a word is a number?
     ck_assert(!check_word(number_word, hashtable));
 }
 END_TEST
